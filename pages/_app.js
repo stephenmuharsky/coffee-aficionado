019d139f955d7a3d60273d14aed219889a2063f5
Stephen Muharsky <smuharsk@gmail.com>
2022-07-30 21:39:53 -0400

    Initial Commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,14 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div>
+      <Component {...pageProps} />
+      {/* <footer>
+        <h2>Made by Stephen</h2>
+      </footer> */}
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
