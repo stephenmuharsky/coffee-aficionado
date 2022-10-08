6780c3424a9fe030212f3632cf65aa66dd511ca4
Stephen Muharsky <smuharsk@gmail.com>
2022-10-08 12:18:28 -0400

    Store Reducer

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,4 @@
-import { createContext } from "react";
+import { createContext, useReducer } from "react";
 import "../styles/globals.css";
 
 const StoreContext = createContext();
@@ -8,14 +8,28 @@ const ACTION_TYPES = {
   SET_COFFEE_STORES: "SET_COFFEE_STORES",
 };
 
+const storeReducer = (state, action) => {
+  switch (action.type) {
+    case ACTION_TYPES.SET_LAT_LONG: {
+      return { ...state, latLong: action.payload.latLong };
+    }
+    case ACTION_TYPES.SET_COFFEE_STORES: {
+      return { ...state, coffeeStores: action.payload.coffeeStores };
+    }
+    default:
+      throw new Error(`Unhandled action type: ${action.type}`);
+  }
+};
+
 const StoreProvider = ({ children }) => {
   const initialState = {
     latLong: "",
     coffeeStores: [],
   };
 
+  const [state, dispatch] = useReducer(storeReducer, initialState);
   return (
-    <StoreContext.Provider value={{ state: initialState }}>
+    <StoreContext.Provider value={{ state, dispatch }}>
       {children}
     </StoreContext.Provider>
   );
